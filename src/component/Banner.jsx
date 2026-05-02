d28a7f7fa4829e8da4b2892c7172a0d1f6ee2892
ahumayanahmed <ahumayan962@gmail.com>
2026-05-02 12:23:59 +0600

    Bannar add

---
+++ src/component/Banner.jsx
@@ -0,0 +1,30 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
+const Banner = () => {
+  return (
+    <div className="bg-[url('https://images.unsplash.com/photo-1505691938895-1758d7feb511?q=80&w=1600&auto=format&fit=crop')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl mt-10">
+      {/* Overlay */}
+      <div className="w-full h-full rounded-lg bg-black/50 flex items-center ">
+        <div className="max-w-7xl mx-auto px-6 text-white">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl">
+            Discover Your <br /> perfect Aesthetic
+          </h1>
+          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
+            Explore our premium collection of tails <br /> designed to elevate youe spase 
+          </p>
+
+          <div className="flex gap-4">
+            <Link href="/allphotos">
+              <Button className="bg-linear-to-r from-pink-500 via-purple-500 bg-red-500">
+               Browse Now
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
