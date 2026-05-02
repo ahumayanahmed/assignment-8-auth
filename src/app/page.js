14b313ee96ce9e5d58cc11b19d55ebc683062e85
ahumayanahmed <ahumayan962@gmail.com>
2026-05-02 12:42:39 +0600

    json and auth and url configure

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Banner from "@/component/Banner";
+import TopGenerations from "@/component/TopGenerations";
+
 
 export default function Home() {
   return (
     <div>
-     
+     <Banner/>
+     <TopGenerations/>
     </div>
   );
 }
